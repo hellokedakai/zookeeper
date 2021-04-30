1187b53102020f358ba6b21b300e9cd08eb9afe0
Kedakai <nicoleteeth@ucla.edu>
2021-04-30 00:44:34 -0700

    get requests to serve page data

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 // parse incoming string or array data
+
+app.use(express.static('public'));
+
+
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
@@ -111,6 +115,23 @@ function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id)[0];
   return result;
 };
+
+// serve (display) index from Express server
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
